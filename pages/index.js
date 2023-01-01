81b7c4ccd7a60c6d8f2326e0d37185d2151b5263
koushith <koushith97@gmail.com>
2023-01-01 18:35:47 +0530

    final site

--- pages/index.js
+++ pages/index.js
@@ -14,9 +14,9 @@ export default function Home() {
         Hey, I’m Preetham Amin
       </h1>
       <h2 className="prose text-gray-600 dark:text-gray-400 mb-6">
-        I am a Bangalore based Product Designer. I love designing and building for the web,
-        you can read more about what I do and things I love over my website
-        preetham.com.
+        I am a Bangalore based Product Designer. I love designing and building
+        for the web, you can read more about what I do and things I love over my
+        website preetham.com.
       </h2>
       {/* <div className='flex space-x-4'>
         <button className='flex items-center justify-center px-4 py-2  bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-gray-100 rounded  '>
